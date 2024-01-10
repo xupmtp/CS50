8210dc2e0efb25d7242cc976d30dfb3f5a60f873
simon10539 <simoniii10539@i-mps.com>
2024-01-10 11:57:10 +0800

    移除ignore檔

--- CS50WEB/pset1/wiki/test.py
+++
@@ -1,4 +0,0 @@
-with open(r'C:\Users\simon\Desktop\import.sql', 'w') as file:
-    for i in range(50000):
-        file.write(f"INSERT INTO IMAGEDETAIL03(workno, businessno, codeno, caseno, fc, qc, indexfield01) "
-                   f"VALUES('01','01','01','{str(i+1).rjust(5, '0')}','Y','QC', '測試資料');\n")
